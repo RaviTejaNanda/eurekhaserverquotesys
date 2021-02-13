e8421800b187181a3d9cb29b554c70a8e9ff61f5
suneetha <nanda.ravi28@gmail.com>
2021-02-12 21:13:44 -0500

    serverApplication

---
+++ src/test/java/comn2s/eurekhaserver/EurekhaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package comn2s.eurekhaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekhaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
